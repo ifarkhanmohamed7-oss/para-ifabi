60e2df80fe99d40f6b5ff631cbc3d4c0a676fc26
ifarkhanmohamed7-oss <ifarkhanmohamed7@gmail.com>
2026-09-09 21:58:03 +0000

    fix api key

--- admin.js
+++ admin.js
@@ -3,8 +3,8 @@
 // ===============================
 
 // حط معلومات Supabase ديالك هنا
-const supabaseUrl = 'https://krtofrjnxqfstmrwymwn.supabase.co/rest/v1/';
-const supabaseKey = 'sb_publishable_7n2MPltuYxtfifSPQ7mxEQ_ZCM11bpe';
+const supabaseUrl = 'https://krtofrjnxqfstmrwymwn.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtydG9mcmpueHFmc3Rtcnd5bXduIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODgxMjA5MDksImV4cCI6MjEwMzY5NjkwOX0.9gwgtXP_zLRhTRW7wDibmAqIak95Pp7BMLjqgmFb-wI';
 
 // إنشاء Supabase client
 window.adminSupabase = window.supabase.createClient(
